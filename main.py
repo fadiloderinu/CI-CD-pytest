46e3ee39d49e6a7176d4e2bb66d2f1598a6402cb
Fadil Oderinu <fadil@oderinu.com>
2025-10-18 16:20:45 -0400

    Fix test assertion for addition and update comment in area test

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from myapp.app import multiply_by_two, divide_by_two
 def main():
     num = int(input("Insert a number: "))
     print("The double of %d is %d" % (num, multiply_by_two(num)))
-    print("The half of %d is %d" % (num, divide_by_two(num)))
+    print("The half of %d is %.2f" % (num, divide_by_two(num)))
     sys.exit(0)
 
 
--- tests/test_area.py
+++ tests/test_area.py
@@ -7,7 +7,7 @@ from myapp.mymodule.funcs import multiply
 def test_area_of_square_last_two_digits():
     """
     Validate area calculation for a square where the expected area
-    is the last two digits of the student ID (78).
+    is the last two digits of my student ID (101000478).
     """
     expected = 78
     side = math.sqrt(expected)
